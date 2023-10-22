5f5ffee14f5f46cb643904814ede50ef75a4def8
Ari Putra Utama <arieputrautama1998@gmail.com>
2023-10-22 20:47:35 +0700

    update

--- classes/SystemSettings.php
+++ classes/SystemSettings.php
@@ -3,38 +3,32 @@ if (!class_exists('DBConnection')) {
     require_once('../config.php');
     require_once('DBConnection.php');
 }
-class SystemSettings extends DBConnection
-{
-    public function __construct()
-    {
+class SystemSettings extends DBConnection{
+    public function __construct(){
         parent::__construct();
     }
-    function check_connection()
-    {
+    function check_connection(){
         return ($this->conn);
     }
-    function load_system_info()
-    {
+    function load_system_info(){
         // if(!isset($_SESSION['system_info'])){
         $sql = "SELECT * FROM system_info";
         $qry = $this->conn->query($sql);
-        while ($row = $qry->fetch_assoc()) {
-            $_SESSION['system_info'][$row['meta_field']] = $row['meta_value'];
-        }
+            while ($row = $qry->fetch_assoc()) {
+                $_SESSION['system_info'][$row['meta_field']] = $row['meta_value'];
+            }
         // }
     }
-    function update_system_info()
-    {
+    function update_system_info(){
         $sql = "SELECT * FROM system_info";
         $qry = $this->conn->query($sql);
-        while ($row = $qry->fetch_assoc()) {
+            while ($row = $qry->fetch_assoc()) {
             if (isset($_SESSION['system_info'][$row['meta_field']])) unset($_SESSION['system_info'][$row['meta_field']]);
             $_SESSION['system_info'][$row['meta_field']] = $row['meta_value'];
         }
         return true;
     }
-    function update_settings_info()
-    {
+    function update_settings_info(){
         $data = "";
         foreach ($_POST as $key => $value) {
             if (!in_array($key, array("about_us", "privacy_policy")))
@@ -79,14 +73,12 @@ class SystemSettings extends DBConnection
             return true;
         }
     }
-    function set_userdata($field = '', $value = '')
-    {
+    function set_userdata($field = '', $value = ''){
         if (!empty($field) && !empty($value)) {
             $_SESSION['userdata'][$field] = $value;
         }
     }
-    function userdata($field = '')
-    {
+    function userdata($field = ''){
         if (!empty($field)) {
             if (isset($_SESSION['userdata'][$field]))
                 return $_SESSION['userdata'][$field];
@@ -96,52 +88,46 @@ class SystemSettings extends DBConnection
             return false;
         }
     }
-    function set_flashdata($flash = '', $value = '')
-    {
+    function set_flashdata($flash = '', $value = ''){
         if (!empty($flash) && !empty($value)) {
             $_SESSION['flashdata'][$flash] = $value;
-            return true;
+        return true;
         }
     }
-    function chk_flashdata($flash = '')
-    {
+    function chk_flashdata($flash = ''){
         if (isset($_SESSION['flashdata'][$flash])) {
             return true;
-        } else {
+        }else {
             return false;
         }
     }
-    function flashdata($flash = '')
-    {
+    function flashdata($flash = ''){
         if (!empty($flash)) {
             $_tmp = $_SESSION['flashdata'][$flash];
             unset($_SESSION['flashdata']);
             return $_tmp;
-        } else {
+        }else {
             return false;
         }
     }
-    function sess_des()
-    {
+    function sess_des(){
         if (isset($_SESSION['userdata'])) {
-            unset($_SESSION['userdata']);
+                unset($_SESSION['userdata']);
             return true;
         }
         return true;
     }
-    function info($field = '')
-    {
+    function info($field = ''){
         if (!empty($field)) {
             if (isset($_SESSION['system_info'][$field]))
                 return $_SESSION['system_info'][$field];
             else
                 return false;
-        } else {
+        }else {
             return false;
         }
     }
-    function set_info($field = '', $value = '')
-    {
+    function set_info($field = '', $value = ''){
         if (!empty($field) && !empty($value)) {
             $_SESSION['system_info'][$field] = $value;
         }
@@ -159,3 +145,4 @@ switch ($action) {
         // echo $sysset->index();
         break;
 }
+?>
--- packages.php
+++ packages.php
@@ -29,30 +29,5 @@
 				if($rate > 0 && $review_count > 0)
 				//$rate = number_format($rate/$review_count,0,"");
 		?>
-			<div class="card d-flex w-100 rounded-0 mb-3 package-item">
-				<img class="card-img-top" src="<?php echo validate_image($cover) ?>" alt="<?php echo $row['title'] ?>" height="200rem" style="object-fit:cover">
-				<div class="card-body">
-				<h5 class="card-title truncate-1"><?php echo $row['title'] ?></h5>
-				<div class=" w-100 d-flex justify-content-start">
-				<form action="">
-					<div class="stars stars-small">
-							<input disabled class="star star-5" id="star-5" type="radio" name="star" <?php echo $rate == 5 ? "checked" : '' ?>/> <label class="star star-5" for="star-5"></label> 
-							<input disabled class="star star-4" id="star-4" type="radio" name="star" <?php echo $rate == 4 ? "checked" : '' ?>/> <label class="star star-4" for="star-4"></label> 
-							<input disabled class="star star-3" id="star-3" type="radio" name="star" <?php echo $rate == 3 ? "checked" : '' ?>/> <label class="star star-3" for="star-3"></label> 
-							<input disabled class="star star-2" id="star-2" type="radio" name="star" <?php echo $rate == 2 ? "checked" : '' ?>/> <label class="star star-2" for="star-2"></label> 
-							<input disabled class="star star-1" id="star-1" type="radio" name="star" <?php echo $rate == 1 ? "checked" : '' ?>/> <label class="star star-1" for="star-1"></label> 
-					</div>
-				</form>
-				</div>
-				<p class="card-text truncate"><?php echo $row['description'] ?></p>
-				<div class="w-100 d-flex justify-content-between">
-					<span class="rounded-0 btn btn-flat btn-sm btn-primary"><i class="fa fa-tag"></i> <?php echo number_format($row['cost']) ?></span>
-					<a href="./?page=view_package&id=<?php echo md5($row['id']) ?>" class="btn btn-sm btn-flat btn-warning">View Package <i class="fa fa-arrow-right"></i></a>
-				</div>
-				</div>
-			</div>
-		<?php endwhile; ?>
-	</div>
-	
-	</div>
+			
 </section>
