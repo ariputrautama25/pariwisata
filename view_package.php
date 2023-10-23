9d427d6fe885e2973e1adaa0323e231ae9e7343b
akimmmm21 <19574008@stmik.jayakarta.ac.id>
2023-10-23 17:37:32 +0700

    update package

--- view_package.php
+++ view_package.php
@@ -6,42 +6,42 @@ if(isset($_GET['id'])){
             $$k = $v;
         }
     }
-$review = $conn->query("SELECT r.*,concat(firstname,' ',lastname) as name FROM `rate_review` r inner join users u on r.user_id = u.id where r.package_id='{$id}' order by unix_timestamp(r.date_created) desc ");
-$review_count =$review->num_rows;
-$rate = 0;
-$feed = array();
-while($row= $review->fetch_assoc()){
-    $rate += $row['rate'];
-    if(!empty($row['review'])){
-        $row['review'] = stripslashes(html_entity_decode($row['review']));
-        $feed[] = $row;
+    $review = $conn->query("SELECT r.*,concat(firstname,' ',lastname) as name FROM `rate_review` r inner join users u on r.user_id = u.id where r.package_id='{$id}' order by unix_timestamp(r.date_created) desc ");
+    $review_count =$review->num_rows;
+    $rate = 0;
+    $feed = array();
+    while($row= $review->fetch_assoc()){
+        $rate += $row['rate'];
+        if(!empty($row['review'])){
+            $row['review'] = stripslashes(html_entity_decode($row['review']));
+            $feed[] = $row;
+        }
     }
-}
-if($rate > 0 && $review_count > 0)
-$rate = number_format($rate/$review_count,0,"");
-$files = array();
-if(is_dir(base_app.'uploads/package_'.$id)){
-    $ofile = scandir(base_app.'uploads/package_'.$id);
-    foreach($ofile as $img){
-        if(in_array($img,array('.','..')))
-        continue;
-        $files[] = validate_image('uploads/package_'.$id.'/'.$img);
+    if($rate > 0 && $review_count > 0)
+    $rate = number_format($rate/$review_count,0,"");
+    $files = array();
+    if(is_dir(base_app.'uploads/package_'.$id)){
+        $ofile = scandir(base_app.'uploads/package_'.$id);
+        foreach($ofile as $img){
+            if(in_array($img,array('.','..')))
+            continue;
+            $files[] = validate_image('uploads/package_'.$id.'/'.$img);
+        }
     }
-}
-}
-?>
-<section class="page-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-5">
-                <div id="tourCarousel"  class="carousel slide" data-ride="carousel" data-interval="3000">
-                    <div class="carousel-inner h-100">
-                        <?php foreach($files as $k => $img): ?>
-                        <div class="carousel-item  h-100 <?php echo $k == 0? 'active': '' ?>">
-                            <img class="d-block w-100  h-100" src="<?php echo $img ?>" alt="">
+    }
+    ?>
+    <section class="page-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div id="tourCarousel"  class="carousel slide" data-ride="carousel" data-interval="3000">
+                        <div class="carousel-inner h-100">
+                            <?php foreach($files as $k => $img): ?>
+                            <div class="carousel-item  h-100 <?php echo $k == 0? 'active': '' ?>">
+                                <img class="d-block w-100  h-100" src="<?php echo $img ?>" alt="">
+                            </div>
+                            <?php endforeach; ?>
                         </div>
-                        <?php endforeach; ?>
-                    </div>
                     <a class="carousel-control-prev" href="#tourCarousel" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="sr-only">Previous</span>
