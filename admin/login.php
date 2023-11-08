7bd8a1600c6f3bc74fc597d2af7de6afdef300ac
akimmmm21 <19574008@stmik.jayakarta.ac.id>
2023-11-08 17:55:48 +0700

    login

---
+++ admin/login.php
@@ -0,0 +1,26 @@
+<?php require_once('../config.php') ?>
+<!DOCTYPE html>
+<html lang="en" class="" style="height: auto;">
+ <?php require_once('inc/header.php') ?>
+<body class="hold-transition login-page">
+  <script>
+    start_loader()
+  </script>
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="./" class="h1"><b>Login</b></a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form id="login-frm" action="" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" name="username" placeholder="Username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
