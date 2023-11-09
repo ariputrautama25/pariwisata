2c5e220a75af5f8aaab529bc2ce70787cb62a82b
Msthf <zuhair20.mz@gmail.com>
2023-11-09 18:42:18 +0700

    penambahan login

--- admin/login.php
+++ admin/login.php
@@ -23,4 +23,42 @@
               <span class="fas fa-user"></span>
             </div>
           </div>
+          </div>
+        <div class="row">
+          <div class="col-8">
+            <a href="<?php echo base_url ?>">Go to Website</a>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
         </div>
+      </form>
+      <!-- /.social-auth-links -->
+
+      <!-- <p class="mb-1">
+        <a href="forgot-password.html">I forgot my password</a>
+      </p> -->
+      
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+
+<script>
+  $(document).ready(function(){
+    end_loader();
+  })
+</script>
+</body>
+</html>
