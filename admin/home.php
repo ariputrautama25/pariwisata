ebd5597f90d13f73c1e72d708900b72a40170442
FachriArdhian <19572003@stmik.jayakarta.ac.id>
2023-11-12 21:10:47 +0700

    update home

--- admin/home.php
+++ admin/home.php
@@ -15,3 +15,21 @@
         }
     }
     ?>
+    <div id="tourCarousel"  class="carousel slide" data-ride="carousel" data-interval="3000">
+      <div class="carousel-inner h-100">
+          <?php foreach($files as $k => $img): ?>
+          <div class="carousel-item  h-100 <?php echo $k == 0? 'active': '' ?>">
+              <img class="d-block w-100  h-100" src="<?php echo $img ?>" alt="">
+          </div>
+          <?php endforeach; ?>
+      </div>
+      <a class="carousel-control-prev" href="#tourCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#tourCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+      </a>
+  </div>
+</div>
