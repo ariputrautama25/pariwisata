83dcade8f5a99a2a109c3919155d7cb8aee6e82e
AndiAlHimam <19572005@stmik.jayakarta.ac.id>
2023-11-15 21:10:04 +0700

    updateheader

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -40,3 +40,17 @@
     <style type="text/css">/* Chart.js */
       @keyframes chartjs-render-animation{from{opacity:.99}to{opacity:1}}.chartjs-render-monitor{animation:chartjs-render-animation 1ms}.chartjs-size-monitor,.chartjs-size-monitor-expand,.chartjs-size-monitor-shrink{position:absolute;direction:ltr;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1}.chartjs-size-monitor-expand>div{position:absolute;width:1000000px;height:1000000px;left:0;top:0}.chartjs-size-monitor-shrink>div{position:absolute;width:200%;height:200%;left:0;top:0}
     </style>
+    <!-- jQuery -->
+    <script src="<?php echo base_url ?>plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="<?php echo base_url ?>plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- SweetAlert2 -->
+    <script src="<?php echo base_url ?>plugins/sweetalert2/sweetalert2.min.js"></script>
+    <!-- Toastr -->
+    <script src="<?php echo base_url ?>plugins/toastr/toastr.min.js"></script>
+    <script>
+        var base_url = '<?php echo base_url ?>';
+    </script>
+    <script src="<?php echo base_url ?>dist/js/script.js"></script>
+
+  </head>
