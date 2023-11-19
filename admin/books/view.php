70568116cbd20e2dd29cffc4a10f631007de904b
AndiAlHimam <19572005@stmik.jayakarta.ac.id>
2023-11-19 21:16:04 +0700

    update

--- admin/books/view.php
+++ admin/books/view.php
@@ -11,3 +11,24 @@ if(isset($_GET['id'])){
     #uni_modal .modal-content>.modal-footer{
         display:none;
     }
+    </style>
+<p><b>Package:</b> <?php echo $title ?></p>
+<p><b>Details:</b> <span class="truncate"><?php echo strip_tags(stripslashes(html_entity_decode($title))) ?></span></p>
+<p><b>Schedule:</b> <?php echo date("F d, Y",strtotime("schedule")) ?></p>
+<form action="" id="book-status">
+    <input type="hidden" name="id" value="<?php echo $id ?>">
+    <div class="form-group">
+        <label for="" class="control-label">Status</label>
+        <select name="status" id="" class="select custom-select">
+            <option value="0" <?php echo $status == 0 ? "selected" : '' ?>>Pending</option>
+            <option value="1" <?php echo $status == 1 ? "selected" : '' ?>>Confimed</option>
+            <option value="2" <?php echo $status == 2 ? "selected" : '' ?>>Cancelled</option>
+            <option value="3" <?php echo $status == 3 ? "selected" : '' ?>>Done</option>
+        </select>
+    </div>
+</form>
+
+<div class="modal-footer">
+<button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Update</button>
+<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+</div>
