2e020d3eb28cb62f2f91604bff1e9c5f2ba74d3c
ZarssssTz <dwitamareza8@gmail.com>
2023-11-28 11:34:30 +0700

    update index-systeminfo

--- admin/system_info/index.php
+++ admin/system_info/index.php
@@ -53,3 +53,29 @@
 	              <label class="custom-file-label" for="customFile">Choose file</label>
 	            </div>
 			</div>
+            <div class="form-group d-flex justify-content-center">
+				<img src="<?php echo validate_image($_settings->info('logo')) ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
+			</div>
+			<div class="form-group">
+				<label for="" class="control-label">Website Cover</label>
+				<div class="custom-file">
+	              <input type="file" class="custom-file-input rounded-circle" id="customFile" name="cover" onchange="displayImg2(this,$(this))">
+	              <label class="custom-file-label" for="customFile">Choose file</label>
+	            </div>
+			</div>
+			<div class="form-group d-flex justify-content-center">
+				<img src="<?php echo validate_image($_settings->info('cover')) ?>" alt="" id="cimg2" class="img-fluid img-thumbnail">
+			</div>
+			</form>
+		</div>
+		<div class="card-footer">
+			<div class="col-md-12">
+				<div class="row">
+					<button class="btn btn-sm btn-primary" form="system-frm">Update</button>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
+
